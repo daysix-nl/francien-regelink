b1fe9b2a36734304d13cafbd445831de6a2a4ce5
svennijhuis <sanijhuis@live.nl>
2023-08-30 10:51:18 +0200

    change script

--- script/index.js
+++ script/index.js
@@ -293,15 +293,18 @@ try {
 } catch (error) {}
 
 try {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-  // We listen to the resize event
-  window.addEventListener("resize", () => {
-    // We execute the same script as before
+  // Function to update the --vh custom property
+  const updateVh = () => {
+    // Get the viewport height
     let vh = window.innerHeight * 0.01;
+    // Set the value of --vh to the root of the document
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
+  };
+
+  // Call the function on page load
+  updateVh();
+
+  // Listen for the resize and scroll events
+  window.addEventListener("resize", updateVh);
+  window.addEventListener("scroll", updateVh);
 } catch (error) {}
